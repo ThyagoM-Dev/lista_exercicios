df2ab53da9f40ef2edd4f48baa65f93537c61bb4
Thyago Marcel <thyagomarcel@outlook.com>
2023-10-23 16:12:57 -0300

    Resp.1

---
+++ Q1.py
@@ -0,0 +1,9 @@
+# Faça um programa que receba três números, calcule e mostre a multiplicação desses números.
+
+print ('\nO sistema mutiplicara os valores adiconados.\n')
+
+a = int(input('Informe o prtrimeiro valor: '))
+b = int(input('Informe o Segundo valor: '))
+c = int(input('Informe o Terceiro valor: '))
+
+print (a * b * c)
