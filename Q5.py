2ec60574dfafc6176c2e3adc525a618d4862a36f
Thyago Marcel <thyagomarcel@outlook.com>
2023-10-31 14:42:01 -0300

    Q5

--- Q5.py
+++ Q5.py
@@ -1,5 +1,18 @@
-# Faça um algoritmo que leia as 3 notas de um aluno e calcule a média final
-# deste aluno. Considerar que a média é ponderada e que o peso das notas é: 2,3
-# e 5, respectivamente.
+# Faça um algoritmo que leia as 3 notas de um aluno e calcule a média final deste aluno.
+# Considerar que a média é ponderada e que o peso das notas é: 2,3 e 5, respectivamente
 
-aluno = celso
+aluno = "carlos"
+primeirobm = (2.3)
+segundobm = (2.3)
+terceirobm = (2.3)
+media = int(5)
+
+totalNota = primeirobm + segundobm + terceirobm
+totalMedia = media
+
+totalfinal = totalNota 
+
+if totalNota >= totalMedia:
+    print ('parabens ' + aluno + ', voce foi aprovado')
+else:
+    print (aluno + ', infelizmente voce foi reprovado')
